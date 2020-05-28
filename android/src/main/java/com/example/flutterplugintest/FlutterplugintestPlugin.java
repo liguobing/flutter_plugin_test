c0759d930f6dd0c94ce05ef11bcc060d22dd26b0
lixyz <29418290@qq.com>
2020-05-29 01:34:15 +0800

    设置返回值，查看 Context 是否为空

--- android/src/main/java/com/example/flutterplugintest/FlutterplugintestPlugin.java
+++ android/src/main/java/com/example/flutterplugintest/FlutterplugintestPlugin.java
@@ -14,75 +14,77 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FlutterplugintestPlugin */
+/**
+ * FlutterplugintestPlugin
+ */
 public class FlutterplugintestPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
 
-  private Context context;
+    private Context context;
 
-  private String resultString = "";
+    private String resultString = "";
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutterplugintest");
-    channel.setMethodCallHandler(this);
-    context = flutterPluginBinding.getApplicationContext();
-    if(context == null){
-      resultString = "IsNull";
-    }else{
-      resultString = "NotNull";
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutterplugintest");
+        channel.setMethodCallHandler(this);
+        context = flutterPluginBinding.getApplicationContext();
+        if (context == null) {
+            resultString = "IsNull";
+        } else {
+            resultString = "NotNull";
+        }
     }
-  }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutterplugintest");
-    channel.setMethodCallHandler(new FlutterplugintestPlugin());
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android： " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+    // plugin registration via this function while apps migrate to use the new Android APIs
+    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+    //
+    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+    // in the same class.
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutterplugintest");
+        channel.setMethodCallHandler(new FlutterplugintestPlugin());
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android： " + resultString + "|" + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
+    }
 
-  @Override
-  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 
-  }
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
 
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
+    }
 
-  }
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
 
-  @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+    }
 
-  }
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
 
-  @Override
-  public void onDetachedFromActivity() {
+    }
 
-  }
+    @Override
+    public void onDetachedFromActivity() {
+
+    }
 }
